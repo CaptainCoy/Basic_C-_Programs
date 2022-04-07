e20f7655aeb65f99c23c20cd60c8639ccfd1e4e6
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2022-04-07 12:50:30 -0600

    commit

---
+++ CodeFirstAssignment/dbo.Students.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [ID]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_dbo.Students] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
